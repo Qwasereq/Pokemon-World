13ab2866f3b5eccac6ed080edebaaa86785b46ef
Restyled.io <commits@restyled.io>
2022-11-17 11:54:56 +0000

    Restyled by clang-format

--- Models/Pokemon.cs
+++ Models/Pokemon.cs
@@ -2,29 +2,12 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace PokemonWorld.Models
-{
-public class Pokemon
-{
-    public int Id {
-        get;
-        set;
-    }
-    public string? Name {
-        get;
-        set;
-    }
-    public DateTime PokemonReleaseDate {
-        get;
-        set;
-    }
-    public string? Genre {
-        get;
-        set;
-    }
-    public decimal Strenght {
-        get;
-        set;
-    }
-}
+namespace PokemonWorld.Models {
+  public class Pokemon {
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public DateTime PokemonReleaseDate { get; set; }
+    public string? Genre { get; set; }
+    public decimal Strenght { get; set; }
+  }
 }
