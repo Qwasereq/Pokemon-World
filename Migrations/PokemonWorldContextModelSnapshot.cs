387c13edfb2abfb3dadbdcd573182b3c4ea3f024
Restyled.io <commits@restyled.io>
2022-11-20 15:28:27 +0000

    Restyled by astyle

--- Migrations/PokemonWorldContextModelSnapshot.cs
+++ Migrations/PokemonWorldContextModelSnapshot.cs
@@ -9,41 +9,41 @@ using PokemonWorld.Data;
 
 namespace PokemonWorld.Migrations
 {
-    [DbContext(typeof(PokemonWorldContext))]
-    partial class PokemonWorldContextModelSnapshot : ModelSnapshot
+[DbContext(typeof(PokemonWorldContext))]
+partial class PokemonWorldContextModelSnapshot : ModelSnapshot
+{
+    protected override void BuildModel(ModelBuilder modelBuilder)
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        modelBuilder
+        .HasAnnotation("ProductVersion", "6.0.1")
+        .HasAnnotation("Relational:MaxIdentifierLength", 128);
+
+        SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder, 1L, 1);
+
+        modelBuilder.Entity("PokemonWorld.Models.Pokemon", b =>
         {
-            modelBuilder
-                .HasAnnotation("ProductVersion", "6.0.1")
-                .HasAnnotation("Relational:MaxIdentifierLength", 128);
+            b.Property<int>("Id")
+            .ValueGeneratedOnAdd()
+            .HasColumnType("int");
 
-            SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder, 1L, 1);
+            SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
 
-            modelBuilder.Entity("PokemonWorld.Models.Pokemon", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
+            b.Property<string>("Genre")
+            .HasColumnType("nvarchar(max)");
 
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
+            b.Property<decimal>("Name")
+            .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("Genre")
-                        .HasColumnType("nvarchar(max)");
+            b.Property<DateTime>("PokemonReleaseDate")
+            .HasColumnType("datetime2");
 
-                    b.Property<decimal>("Name")
-                        .HasColumnType("nvarchar(max)");
+            b.Property<string>("Strenght")
+            .HasColumnType("decimal(2,10)");
 
-                    b.Property<DateTime>("PokemonReleaseDate")
-                        .HasColumnType("datetime2");
+            b.HasKey("Id");
 
-                    b.Property<string>("Strenght")
-                        .HasColumnType("decimal(2,10)");
-
-                    b.HasKey("Id");
-
-                    b.ToTable("Pokemon");
-                });
-        }
+            b.ToTable("Pokemon");
+        });
     }
+}
 }
