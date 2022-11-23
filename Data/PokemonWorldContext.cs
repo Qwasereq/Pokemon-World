cf1cbaf10c43fb5346db126692ed8af27ce2c4d8
MateuszLuckos <luckosmateusz@gmail.com>
2022-11-23 08:54:09 +0000

    Data Context

---
+++ Data/PokemonWorldContext.cs
@@ -0,0 +1,20 @@
+#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PokemonWorld.Models;
+
+namespace PokemonWorld.Data
+{
+    public class PokemonWorldContext : DbContext
+    {
+        public PokemonWorldContext (DbContextOptions<PokemonWorldContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PokemonWorld.Models.Pokemon> Pokemon { get; set; }
+    }
+}
